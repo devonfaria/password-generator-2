b39e7050c43de931c8f96b89a6c85aac51c631e0
Devon Faria <devonfaria@gmail.com>
2022-02-28 15:18:48 -0500

    added four strings for all possible character types and options

--- assets/js/script.js
+++ assets/js/script.js
@@ -20,10 +20,11 @@ generateBtn.addEventListener("click", writePassword);
 
 function generatePassword() {
   var count = Number(prompt("Please enter the amount of characters you would like in your password."));
-  
-  var letters = "abcdefghijklmnopqrstuvwxyz";
+  var lowercaseLetters = "abcdefghijklmnopqrstuvwxyz";
+  var uppercaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
   // Add special characters
-  var specialCharacters = "";
+  var specialCharacters = "!#$%&'()*+,-./:;<=>?@[^_`{|}~";
+  var numbers = "1234567890";
   var genPassword = "";
 
   for (var i = 0; i < count; i++) {
