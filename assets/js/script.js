b99659c4755cbe9fb1c0c271a02094d79a2e8fae
Devon Faria <devonfaria@gmail.com>
2022-02-28 15:30:10 -0500

    added prompts for adding character types to generated password

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,23 +8,30 @@ function writePassword() {
   passwordText.value = password;
 }
 
-function writePassword () {
-  console.log('Button is being clicked!');
-}
-;
+// function writePassword () {
+//   console.log('Button is being clicked!');
+// }
+// ;
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", generatePassword());
 
 
 
 function generatePassword() {
   var count = Number(prompt("Please enter the amount of characters you would like in your password."));
+  prompt("Would you like to include uppercase letters?");
+  prompt("Would you like to include lowercase letters?");
+  prompt("Would you like to include special characters?");
+  prompt("Would you like to include numbers?");
+
+  // List of possible charcter types and the options
   var lowercaseLetters = "abcdefghijklmnopqrstuvwxyz";
   var uppercaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  // Add special characters
   var specialCharacters = "!#$%&'()*+,-./:;<=>?@[^_`{|}~";
   var numbers = "1234567890";
+
+  // Empty string to collect the chosen character types
   var genPassword = "";
 
   for (var i = 0; i < count; i++) {
