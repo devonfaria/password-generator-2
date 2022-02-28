1be69754e0e54b93dff000d92ea3eda0bfeb76ac
Devon Faria <devonfaria@gmail.com>
2022-02-28 15:13:32 -0500

    created 'Generate Password' button functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,10 +5,33 @@ var generateBtn = document.querySelector("#generate");
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
-
 }
+
+function writePassword () {
+  console.log('Button is being clicked!');
+}
+;
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+
+function generatePassword() {
+  var count = Number(prompt("Please enter the amount of characters you would like in your password."));
+  
+  var letters = "abcdefghijklmnopqrstuvwxyz";
+  // Add special characters
+  var specialCharacters = "";
+  var genPassword = "";
+
+  for (var i = 0; i < count; i++) {
+    var random = Math.floor(Math.random() * letters.length);
+    genPassword += letters[random];
+  }
+
+  console.log(genPassword);
+
+  return genPassword;
+}
